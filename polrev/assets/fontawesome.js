b0ce9814166319866fd496a19e07cb25a37b4750
Kurtis Fields <kurtisfields@gmail.com>
2021-12-14 17:09:34 -0700

    fix: only use necessary fontawesome icons

--- polrev/assets/fontawesome.js
+++ polrev/assets/fontawesome.js
@@ -1,33 +1,49 @@
-/*
-export interface Config {
-  familyPrefix: IconPrefix;
-  replacementClass: string;
-  autoReplaceSvg: boolean | 'nest';
-  autoAddCss: boolean;
-  autoA11y: boolean;
-  searchPseudoElements: boolean;
-  observeMutations: boolean;
-  keepOriginalSource: boolean;
-  measurePerformance: boolean;
-  showMissingIcons: boolean;
-}
-*/
-
-FontAwesomeConfig = { searchPseudoElements: true }
-
 import { library, dom } from '@fortawesome/fontawesome-svg-core'
-/*
-import { faSearch, faUserCircle} from "@fortawesome/free-solid-svg-icons";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
-library.add(faSearch, faUserCircle, faGithub);
 
-*/
-import { fas } from '@fortawesome/free-solid-svg-icons'
+/*import { fas } from '@fortawesome/free-solid-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
-
-// Add all icons to the library so you can use it in your page
-//library.add(FontAwesomeConfig, fas, far, fab)
 library.add(fas, far, fab)
+*/
+
+import {
+  faUser,
+  faUserCircle,
+  faSearch,
+  faCalendar,
+  faFolderOpen,
+  faTag,
+  faComments,
+} from "@fortawesome/free-solid-svg-icons";
+
+import {
+  faGithub,
+  faFacebook,
+  faTwitter,
+  faReddit,
+  faMeetup,
+  faSlack,
+  faTrello,
+  faLinkedin
+} from "@fortawesome/free-brands-svg-icons";
+
+library.add(
+  faUser,
+  faUserCircle,
+  faSearch,
+  faCalendar,
+  faFolderOpen,
+  faTag,
+  faComments,
+
+  faGithub,
+  faFacebook, 
+  faTwitter,
+  faReddit,
+  faMeetup,
+  faSlack,
+  faTrello,
+  faLinkedin
+  );
 
 dom.i2svg();
