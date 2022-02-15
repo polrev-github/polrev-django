51a49ab2db1d3fbd807d1219b18c431dded3cfec
kfields <kurtisfields@gmail.com>
2022-02-15 01:39:43 -0700

    feat: multistage docker build

---
+++ polrev/docker-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+# activate our virtual environment here
+#. /opt/pysetup/.venv/bin/activate
+
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput --clear
+gunicorn polrev.wsgi:application
+
+# You can put other setup logic here
+
+# Evaluating passed command:
+exec "$@"
--- polrev/entrypoint.sh
+++
@@ -1,3 +0,0 @@
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput --clear
-gunicorn polrev.wsgi:application
