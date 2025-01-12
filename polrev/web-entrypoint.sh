ae3307527b556d2b3af80e3b8f41dc414eaa8ea4
kfields <kurtisfields@gmail.com>
2025-01-12 15:54:56 -0700

    don't collect static on django container start

--- polrev/web-entrypoint.sh
+++ polrev/web-entrypoint.sh
@@ -6,7 +6,7 @@ set -e
 #. /opt/pysetup/.venv/bin/activate
 
 python manage.py migrate --noinput
-python manage.py collectstatic --noinput --clear
+#python manage.py collectstatic --noinput --clear
 gunicorn polrev.wsgi:application
 
 # You can put other setup logic here
