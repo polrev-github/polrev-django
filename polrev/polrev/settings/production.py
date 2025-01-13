1101b706060d11e74c124969a66267287ce000ca
kfields <kurtisfields@gmail.com>
2025-01-12 23:41:52 -0700

    fix csrf issue

--- polrev/polrev/settings/production.py
+++ polrev/polrev/settings/production.py
@@ -13,6 +13,8 @@ DEBUG = False
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
+CSRF_TRUSTED_ORIGINS = ['https://*.pol-rev.com']
+
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ['*']
 
