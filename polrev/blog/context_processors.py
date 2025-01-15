690bfc08d9e713c5784aa6ce62049f8233dc2c1f
kfields <kurtisfields@gmail.com>
2025-01-15 09:17:43 -0700

    fix site-header

--- polrev/blog/context_processors.py
+++ polrev/blog/context_processors.py
@@ -7,26 +7,8 @@ def blog(request):
         'featured_articles': EntryPage.objects.live().filter(featured=True)
     }
 
-'''
-def header_image(request):
-    return {'header_image': 'img/site-header-cropped.jpg'}
-'''
-
-'''
-def header_image(request):
-    return {'header_image': 'img/site-header-tall.jpg'}
-'''
-
 def header_image(request):
     if request.path != '/' and request.user_agent.is_pc:  # Home page
-        return {'header_image': 'img/site-header.jpg'}
-    else:  # Other pages
-        return {'header_image': 'img/site-header-tall.jpg'}
-
-'''
-def header_image(request):
-    if request.path == '/':  # Home page
-        return {'header_image': 'img/site-header-tall.jpg'}
+        return {'header_image': 'img/site-header-short.jpg'}
     else:  # Other pages
         return {'header_image': 'img/site-header.jpg'}
-'''
