75a8debcc912471d17b733af802e4eddfb0cfa17
kfields <kurtisfields@gmail.com>
2025-10-10 11:29:42 -0600

    make privacy policy an InfoPage

--- polrev/home/urls.py
+++ polrev/home/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("about-us/", views.about, name="about"),
     path("issues/", views.issues, name="issues"),
     path("transparency/", views.transparency, name="transparency"),
-    path("privacy-policy/", views.privacy, name="privacy"),
+    #path("privacy-policy/", views.privacy, name="privacy"),
 ]
